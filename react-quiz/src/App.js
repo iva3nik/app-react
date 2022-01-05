d06871aed30c7e255543ed96a184e150107f11ef
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2022-01-05 23:20:57 +0300

    feat: make the main page

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,12 @@
+import React, {Component} from 'react'
+import Layout from './hoc/Layout/Layout';
+import Quiz from './container/Quiz/Quiz'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <Layout>
+      <Quiz />
+    </Layout>
   );
 }
 
---
+++ react-quiz/src/container/Quiz/Quiz.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import s from './Quiz.module.css'
+
+class Quiz extends Component {
+  render() {
+    return (
+      <div className={s.Quiz}>
+        <h1>Quiz</h1>
+      </div>
+    )
+  }
+}
+
+export default Quiz
