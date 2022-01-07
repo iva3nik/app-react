9b1bbad0e2f7501d3b40254d4877c033ccd3d5de
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2022-01-07 23:24:03 +0300

    feat: make list test

--- react-quiz/src/container/QuizList/QuizList.js
+++ react-quiz/src/container/QuizList/QuizList.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import s from './QuizList.module.css'
 
 const QuizList = props => {
+  const renderQuizes = () => {
+    return [1, 2, 3].map((quiz, index) => {
+      return (
+        <li
+          key={index}
+        >
+          <NavLink to={'/quiz/' + quiz}>
+            Тест {quiz}
+          </NavLink>
+        </li>
+      )
+    })
+  }
+
   return (
-    <div>
-      <h1>Quiz List</h1>
+    <div className={s.QuizList}>
+      <div>
+        <h1>Список тестов</h1>
+
+        <ul>
+          {renderQuizes()}
+        </ul>
+      </div>
     </div>
   )
 }
