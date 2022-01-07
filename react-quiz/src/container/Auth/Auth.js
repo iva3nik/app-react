641054145d4f9565f74e69345f71f19b69e2fb8a
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2022-01-07 23:53:33 +0300

    feat: make the page Auth

--- react-quiz/src/container/Auth/Auth.js
+++ react-quiz/src/container/Auth/Auth.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import s from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 const Auth = props => {
+  const loginHandler = () => {
+
+  }
+
+  const registerHandler = () => {
+
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className={s.Auth}>
+      <div>
+        <h1>Авторизация</h1>
+
+        <form onSubmit={submitHandler} className={s.AuthForm}>
+          <input type='text' />
+          <input type='text' />
+
+          <Button
+            type='success'
+            onClick={loginHandler}
+          >
+            Войти
+          </Button>
+          <Button
+            type='primary'
+            onClick={registerHandler}
+          >
+            Зарегестрироваться
+          </Button>
+        </form>
+      </div>
     </div>
   )
 }
