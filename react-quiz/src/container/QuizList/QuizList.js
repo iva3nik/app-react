8b6ab09a9f037e94875e2938bebc4b513b7601cf
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2022-01-09 22:05:12 +0300

    feat: load quizList

--- react-quiz/src/container/QuizList/QuizList.js
+++ react-quiz/src/container/QuizList/QuizList.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './QuizList.module.css'
+import axios from 'axios'
 
 const QuizList = props => {
+  const [quizes, setQuizes] = useState([])
+
+  useEffect(() => {
+    const response = axios.get('https://quiz-react-c5b2b-default-rtdb.firebaseio.com/quizes.json')
+      response
+        .then((res) => {
+          const quizesList = []
+          Object.keys(res.data).forEach((key, index) => {
+            quizesList.push({
+              id: key,
+              name: `Test N${index + 1}`
+            })
+          })
+          setQuizes(quizesList)
+        })
+  }, [])
+
   const renderQuizes = () => {
-    return [1, 2, 3].map((quiz, index) => {
+    return quizes.map((quiz) => {
       return (
         <li
-          key={index}
+          key={quiz.id}
         >
-          <NavLink to={'/quiz/' + quiz}>
-            Тест {quiz}
+          <NavLink to={'/quiz/' + quiz.id}>
+            {quiz.name}
           </NavLink>
         </li>
       )
